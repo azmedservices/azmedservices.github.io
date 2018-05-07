111f626a72f2f73a09148bd4941d09b233254945
William Robertson <williamp.robertson@gmail.com>
2018-05-07 11:32:46 -0700

    scrolling fix on test links

--- js/main.js
+++ js/main.js
@@ -18,16 +18,16 @@
 
   ///////////////////////////
   // Smooth scroll
-  $("#nav .main-nav a[href^='#']").on("click", function(e) {
-    e.preventDefault();
-    var hash = this.hash;
-    $("html, body").animate(
-      {
-        scrollTop: $(this.hash).offset().top
-      },
-      600
-    );
-  });
+  // $("#nav .main-nav a[href^='#']").on("click", function(e) {
+  //   e.preventDefault();
+  //   var hash = this.hash;
+  //   $("html, body").animate(
+  //     {
+  //       scrollTop: $(this.hash).offset().top
+  //     },
+  //     600
+  //   );
+  // });
 
   $("#back-to-top").on("click", function() {
     $("body,html").animate(
